10957f0878769f2a166438e3c7c99aebca9015c7
Jovana Milojevic <54934370+jojo-dev@users.noreply.github.com>
2019-12-19 04:33:51 +0100

    final commit 3

--- contactForm.js
+++ contactForm.js
@@ -1,20 +1,20 @@
-
 var myform = $("form#myform");
-myform.submit(function(event){
-	event.preventDefault();
+myform.submit(function(event) {
+  event.preventDefault();
 
-  
   var service_id = "gmail";
   var template_id = "contactFormMilojevicDesignCV";
 
   myform.find("button").text("Sending...");
-  emailjs.sendForm(service_id,template_id,myform[0])
-  	.then(function(){ 
-    	alert("Sent!");
-       myform.find("button").text("Send");
-    }, function(err) {
-       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
-       myform.find("button").text("Send");
-    });
+  emailjs.sendForm(service_id, template_id, myform[0]).then(
+    function() {
+      alert("Sent!");
+      myform.find("button").text("Send");
+    },
+    function(err) {
+      alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+      myform.find("button").text("Send");
+    }
+  );
   return false;
 });
