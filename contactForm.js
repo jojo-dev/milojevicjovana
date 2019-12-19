7d5afb369cf903c55e290462d96b4ecba10248ed
Jovana Milojevic <54934370+jojo-dev@users.noreply.github.com>
2019-12-19 13:03:22 +0100

    Final commit 4

--- contactForm.js
+++ contactForm.js
@@ -1,20 +1,51 @@
-var myform = $("form#myform");
-myform.submit(function(event) {
-  event.preventDefault();
+// POST https://api.emailjs.com/api/v1.0/email/send-form
 
-  var service_id = "gmail";
-  var template_id = "contactFormMilojevicDesignCV";
+$("#myForm").on("submit", function(event) {
+  event.preventDefault(); // prevent reload
 
-  myform.find("button").text("Sending...");
-  emailjs.sendForm(service_id, template_id, myform[0]).then(
-    function() {
-      alert("Sent!");
-      myform.find("button").text("Send");
-    },
-    function(err) {
-      alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
-      myform.find("button").text("Send");
-    }
-  );
-  return false;
+  var formData = new FormData(this);
+  formData.append("service_id", "gmail");
+  formData.append("template_id", "YcontactFormMilojevicDesignCV");
+  formData.append("user_id", "user_Dj0LJWxQ0nlWoFfOXPcDm");
+
+  $.ajax("https://api.emailjs.com/api/v1.0/email/send-form", {
+    type: "POST",
+    data: formData,
+    contentType: false, // auto-detection
+    processData: false // no need to parse formData to string
+  })
+    .done(function() {
+      alert("Your mail is sent!");
+    })
+    .fail(function(error) {
+      alert("Oops... " + JSON.stringify(error));
+    });
 });
+
+// var myform = $("form#myform");
+// myform.submit(function(event) {
+//   event.preventDefault();
+
+//   var params = myform.serializeArray().reduce(function(obj, item) {
+//     obj[item.name] = item.value;
+//     return obj;
+//   }, {});
+
+//   // Change to your service ID, or keep using the default service
+//   var service_id = "gmail";
+
+//   var template_id = "contactFormMilojevicDesignCV";
+//   myform.find("button").text("Sending...");
+//   emailjs.send(service_id, template_id, params).then(
+//     function() {
+//       alert("Sent!");
+//       myform.find("button").text("Send");
+//     },
+//     function(err) {
+//       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+//       myform.find("button").text("Send");
+//     }
+//   );
+
+//   return false;
+// });
